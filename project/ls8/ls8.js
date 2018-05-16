f9f587b5f4cc6b3c9c986c58d5f3de6ceae170ee
Cristina Laryea <crispad06@gmail.com>
2018-05-16 13:18:59 -0400

    added coded needed for ls8 file

--- project/ls8/ls8.js
+++ project/ls8/ls8.js
@@ -9,15 +9,28 @@ const CPU = require('./cpu');
 function loadMemory() {
 
     // Hardcoded program to print the number 8 on the console
+    const program = [];
+    const argv = process.argv;
 
-    const program = [ // print8.ls8
-        "10011001", // LDI R0,8  Store 8 into R0
-        "00000000",
-        "00001000",
-        "01000011", // PRN R0    Print the value in R0
-        "00000000",
-        "00000001"  // HLT       Halt and quit
-    ];
+        if(argv.length !== 3) {
+            console.error('usage: node ls8 [filename]');
+            process.exit(1);        
+        }
+    const filename = argv[2];
+    const filedata = fs.readFileSync(filename, 'utf8');
+    const lines = filedata.trim().split(/[\r\n]+/g);
+
+    for(let line of lines) {
+        const cmt = line.indexOf('#');
+        if (cmt !== -1) {
+            line = line.substract(0,cmt);
+        }
+        if (line === '') {
+            continue;
+        }
+        line = line.trim();
+        program.push(line);
+    }
 
     // Load the program into the CPU's memory a byte at a time
     for (let i = 0; i < program.length; i++) {
